54b99db5395e4ca584f45d2f956c432bd0bf7986
Sơn Đoàn <sondoan1204@gmail.com>
2017-05-18 22:38:35 +0700

    config public

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = function () {
   var http = require('http');
   var express = require('express');
   var app = express();
-
+  app.use(express.static(__dirname + '/public'));
   app.set('views', __dirname + '/views/pages');
   app.engine('html', require('ejs').renderFile);
   app.get('/', function(req, res) {
